e71329329fd91efadd0fbc0e692fc781af109837
Noble <cyprianvincent777@gmail.com>
2026-06-21 09:50:42 +0100

    Allow EmailJS config file

---
+++ JS/emailjs-config.js
@@ -0,0 +1,7 @@
+window.EMAILJS_CONFIG = Object.freeze({
+    serviceId: "service_4ym5noi",
+    templateId: "template_izunull",
+    publicKey: "0i6bGwmm1Q77vf-sq",
+    rateLimitMs: 30000,
+    blockedEmails: []
+});
